2fe91530e7864d8836616cf2819f634f6f7ed2f4
sahilm2002 <sahil.mb@gmail.com>
2025-07-11 14:02:19 -0700

    Update src/app/page.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           priority
         />
         <ol clssName="list-inide list-decimal text-sm/6 text-cener sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li clssName="mb-2 tracking-[-.01em]">
+          <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
