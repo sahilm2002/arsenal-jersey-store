8d265c26da96d1f2c9be2ed9910742da5633e330
sahilm2002 <sahil.mb@gmail.com>
2025-07-11 14:29:26 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Gist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Gist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
